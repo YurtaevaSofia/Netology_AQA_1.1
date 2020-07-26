d0cab12fd3f80fd4d61b386652d394cfca20a630
coursar <Sonia@MacBook-Air-Sofa.local>
2020-07-26 15:23:39 +0300

    Test with 1000 added, falls

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -30,15 +30,15 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
 
-//    @Test
-//    public void shouldCountWhen1000() {
-//
-//        CashbackHackService service = new CashbackHackService();
-//        int amount = 1000;
-//
-//        int actual = service.remain(amount);
-//        int expected = 0;
-//
-//        assertEquals(actual, expected);
-//    }
+    @Test
+    public void shouldCountWhen1000() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
 }
