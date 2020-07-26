46d0146f696ea4b2e83b7c0abeff6010dceb388f
coursar <Sonia@MacBook-Air-Sofa.local>
2020-07-26 15:31:03 +0300

    Tests added, third falls

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,38 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void remain() {
+    public void shouldCountWhenLessThan1000() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+
+        int actual = service.remain(amount);
+        int expected = 200;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCountWhenMoreThan1000() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1200;
+
+        int actual = service.remain(amount);
+        int expected = 800;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCountWhen1000() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
     }
 }
