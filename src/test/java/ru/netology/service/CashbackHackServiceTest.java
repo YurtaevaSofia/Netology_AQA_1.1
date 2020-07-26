c5f3287d4d3567cb38526038cd2062a7aac41c25
coursar <Sonia@MacBook-Air-Sofa.local>
2020-07-26 15:19:42 +0300

    Tests without 1000 added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldCountWhenLessThan1000() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+
+        int actual = service.remain(amount);
+        int expected = 200;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCountWhenMoreThan1000() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1200;
+
+        int actual = service.remain(amount);
+        int expected = 800;
+
+        assertEquals(actual, expected);
+    }
+
+//    @Test
+//    public void shouldCountWhen1000() {
+//
+//        CashbackHackService service = new CashbackHackService();
+//        int amount = 1000;
+//
+//        int actual = service.remain(amount);
+//        int expected = 0;
+//
+//        assertEquals(actual, expected);
+//    }
+}
